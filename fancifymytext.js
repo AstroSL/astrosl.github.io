f889c6bdbb260e0e2cabf18150a404ff3a5cac9b
AstroSL <80645683+AstroSL@users.noreply.github.com>
2022-02-02 21:06:47 -0800

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,36 +1,34 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Fancify Shamcify</title>
+function hello() {
+   hello("Hello, world!"); 
+}
 
-		<!-- link to your script file here -->
-		<script type"text/javascript" src="fancifymytext.js"></script>
-        <!--<script> alert("Hello, world!")</script>-->
-	</head>
+//bigger
+function big() {
+   var text = document.getElementById("text");
+   text.style.fontSize = "24pt";
+}
 
-	<body>
-		<h1>Fancify my Text</h1>
-		<!-- Your UI controls go here -->
-        <!--Field Box 1-->
-		<form>
-            <fieldset>
-                <legend>Text</legend>
-			    <label for="Input"><!--Text--></label>
-			    <textarea id="feedme" type="text" rows="4" cols="30" wrap="hard" ></textarea>
-            </fieldset>
-		</form>
-        <!--Field Box 2-->
-        <form>
-            <fieldset>
-                <legend>Fancify</legend>
-                <label for="Buttons"><!--Fancify--></label>
-                <button type ="button" value="Bigger!" onclick="big()">Bigger!</button>
-                <input type="radio" name="FancyShmancy" value="FancyShmancy" onchange="bold()"> FancyShmancy
-                <input type="radio" name="BoringBetty" value="BoringBetty" onchange="bbetty()"> BoringBetty
-                <button type="button" name="Moo" value="moo" onclick="moo()">Moo</button>
-            </fieldset>  
-        </form>
+//bold
+function bold() {
+   var text = document.getElementById("text");
+   text.style.fontWeight = "bold";
+   text.style.color= "blue";
+   text.style.textDecoration= "underline";
+}
 
-	</body>
+//unbold
+function bbetty() {
+   var text = document.getElementById("text");
+   text.style.fontSize = "initial";
+   text.style.fontWeight = "normal";
+   text.style.color = "black";
+   text.style.textDecoration= "none";
+}
 
-    </html>
+//uppercase+ -Moo suffix
+function moo() {
+   var suffix = '-Moo',
+   txt = document.getElementById('text');
+   txt.style.textTransform="uppercase";
+   txt.value = txt.value.split('.').join('') + suffix;
+}
